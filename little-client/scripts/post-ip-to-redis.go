ac85ca905a6bd57a02ce468824e3db5a48ec481f
Russell Endicott <russell.endicott@ge.com>
2017-03-20 17:25:56 -0400

    Adding some comments to the go code

--- little-client/scripts/post-ip-to-redis.go
+++ little-client/scripts/post-ip-to-redis.go
@@ -1,3 +1,13 @@
+/* post-ip-to-redis.go
+Finds the local ip address for eth0 and posts it to a redis instance
+given as param1 and port given as param2. It pushes it to the 
+'ipaddrs' list in redis on DB0. 
+
+Usage:
+  go build post-ip-to-redis.go
+  ./post-ip-to-redis 10.8.1.3 6379
+*/
+
 package main
 
 import "gopkg.in/redis.v5"
@@ -5,11 +15,11 @@ import "net"
 import "fmt"
 import "os"
 
-/*
-URL: https://github.com/mccoyst/myip/blob/master/myip.go
-URL: http://changsijay.com/2013/07/28/golang-get-ip-address/
-*/
 
+// getIp grabs the ipv4 address of the eth0 adapter and returns
+// it as a string. Shamefully stolen from here:
+// URL: https://github.com/mccoyst/myip/blob/master/myip.go
+// URL: http://changsijay.com/2013/07/28/golang-get-ip-address/
 func getIp() string {
 	var istring string
 	addrs, err := net.InterfaceAddrs()
@@ -29,7 +39,11 @@ func getIp() string {
 	return istring
 }
 
+// redisPost takes an ip address as a string and posts it to the
+// redis server and port given as parameters two and three.
+// Inspired by the 'gopkg.in/redis.v5' README
 func redisPost(ip, rip, rport string) {
+	// first setup the redis client info
 	client := redis.NewClient(&redis.Options{
         Addr:     rip + ":" + rport,
         Password: "", // no password set
@@ -40,13 +54,15 @@ func redisPost(ip, rip, rport string) {
     fmt.Println(pong, err1)
     // Output: PONG <nil>
     fmt.Println("IP Addr" + ip)
+	// push the ipaddress to the 'ipaddrs' list with Lpush
     err2 := client.LPush("ipaddrs", ip).Err()
     if err2 != nil {
         panic(err2)
     }
 }
 
-
+// main takes first two args from cmdline and spits some
+// messages to stdout.
 func main() {
 	redisIp := os.Args[1]
 	redisPort := os.Args[2]
